3a2000248dd2f9ac85fac91f40743d4b4d077b91
rejth <kirsanovia@yandex.ru>
2020-09-21 21:04:09 +0300

    update: added slider

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
