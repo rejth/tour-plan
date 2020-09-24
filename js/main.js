56660e4e51e84c4f88672b5b5afa85a17290423b
rejth <kirsanovia@yandex.ru>
2020-09-24 23:29:34 +0300

    update: finished reviews block

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.hotel-slider__container', {
+var hotelSlider = new Swiper('.hotel-slider__container', {
   // Optional parameters
   loop: true,
 
@@ -6,6 +6,16 @@ var mySwiper = new Swiper('.hotel-slider__container', {
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow"
+  }
+});
+
+var reviewSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  }
 })
