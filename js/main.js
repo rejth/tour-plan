5475bd47ff92c1656e32120ece476f658a4d4ce7
rejth <kirsanovia@yandex.ru>
2020-10-05 23:30:19 +0300

    update: finished adaptive for modal window

--- js/main.js
+++ js/main.js
@@ -42,4 +42,33 @@ $(document).ready(function () {
       .querySelector("body")
       .classList.toggle("no-scroll");
   });
+
+  // View other options button to call modal window
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  // var ESCCloseButton = $("[data-toggle=onkeydown]");
+  modalButton.on("click", openModalWindow);
+  closeModalButton.on("click", closeModalWindow);
+  // ESCCloseButton.on("keydown", ESCloseModalWindow);
+
+  function openModalWindow() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  };
+
+  function closeModalWindow(event) {
+    event.preventDefault()
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  };
+
+  // function ESCloseModalWindow(event) {
+  //   if (event.keyCode == 27)
+  //     console.log("scsdc")
+  // };
+
 });
