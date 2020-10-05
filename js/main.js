a2f3ab4234bdd44191753e9724fe98b8dd871617
rejth <kirsanovia@yandex.ru>
2020-10-05 22:01:33 +0300

    create: created _modal.sass

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
-// Hotel slider
-var hotelSlider = new Swiper('.hotel-slider__container', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  keyboard: {
-    enabled: true,
-  },
+$(document).ready(function () {
+  // Hotel slider
+  var hotelSlider = new Swiper('.hotel-slider__container', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    keyboard: {
+      enabled: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: "coverflow"
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: "coverflow"
+  });
 
-// Review slider
-var reviewSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  keyboard: {
-    enabled: true,
-  },
+  // Review slider
+  var reviewSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    keyboard: {
+      enabled: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  }
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    }
+  });
 
-var menuButton = document.querySelector(".menu-button")
-menuButton.addEventListener("click", function () {
-  document
-    .querySelector(".navbar-menu")
-    .classList.toggle("navbar-menu--visible");
-  document
-    .querySelector("body")
-    .classList.toggle("no-scroll");
+  // Menu Button
+  var menuButton = document.querySelector(".menu-button")
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".navbar-menu")
+      .classList.toggle("navbar-menu--visible");
+    document
+      .querySelector("body")
+      .classList.toggle("no-scroll");
+  });
 });
