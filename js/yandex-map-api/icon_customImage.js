6732f2f9f2503c3d20d3082fd17cfdb4bc93b2ff
rejth <kirsanovia@yandex.ru>
2020-10-07 23:06:25 +0300

    fix: optimization

--- js/yandex-map-api/icon_customImage.js
+++ js/yandex-map-api/icon_customImage.js
@@ -1,4 +1,11 @@
-ymaps.ready(function () {
+setTimeout(function () {
+  var elem = document.createElement('script');
+  elem.type = 'text/javascript';
+  elem.src = '//api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-RU&onload=getYaMap';
+  document.getElementsByTagName('body')[0].appendChild(elem);
+}, 2000);
+
+function getYaMap() {
   var myMap = new ymaps.Map('map', {
       center: [7.838252, 98.29922],
       zoom: 9
@@ -24,4 +31,32 @@ ymaps.ready(function () {
 
   myMap.geoObjects
     .add(myPlacemark);
-});
+}
+
+// ymaps.ready(function () {
+//   var myMap = new ymaps.Map('map', {
+//       center: [7.838252, 98.29922],
+//       zoom: 9
+//     }, {
+//       searchControlProvider: 'yandex#search'
+//     }),
+
+//     myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+//       hintContent: 'Grand Hotel',
+//       balloonContent: 'The awesome place in the world!'
+//     }, {
+//       // Опции.
+//       // Необходимо указать данный тип макета.
+//       iconLayout: 'default#image',
+//       // Своё изображение иконки метки.
+//       iconImageHref: './img/myIcon.gif',
+//       // Размеры метки.
+//       iconImageSize: [30, 42],
+//       // Смещение левого верхнего угла иконки относительно
+//       // её "ножки" (точки привязки).
+//       iconImageOffset: [-5, -38]
+//     });
+
+//   myMap.geoObjects
+//     .add(myPlacemark);
+// });
